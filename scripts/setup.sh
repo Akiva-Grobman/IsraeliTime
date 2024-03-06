5366b149cfa9d439d10ecc1ac31fbf940be5a3b4
Akiva Grobman <akiva.grobman@gmail.com>
2024-03-06 18:56:16 +0200

    Use logger cofnig

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+# Setup
+mkdir -p /var/log/fastapi
+
 # Install dependencies
 sudo /usr/bin/python3 -m pip install -r /home/ubuntu/IsraeliTime/python-src/requirements.txt
 
 # Setup service
-ORIGINAL_USER=`whoami`
 sudo su
 cp server-side.service /etc/systemd/system/
 systemctl daemon-reload
 systemctl enable test.service
 systemctl start test.service
-su $ORIGINAL_USER
+su ubuntu
