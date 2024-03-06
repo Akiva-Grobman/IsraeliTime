f7a5dbb6a1523c4ecbefab812f64d889c8b05efd
Akiva Grobman <akiva.grobman@gmail.com>
2024-03-06 18:44:54 +0200

    Fix service config

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+# Install dependencies
+sudo /usr/bin/python3 -m pip install -r /home/ubuntu/IsraeliTime/python-src/requirements.txt
+
+# Setup service
+ORIGINAL_USER=`whoami`
 sudo su
 cp server-side.service /etc/systemd/system/
+systemctl daemon-reload
 systemctl enable test.service
 systemctl start test.service
+su $ORIGINAL_USER
