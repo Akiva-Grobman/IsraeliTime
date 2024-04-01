1cf1a699b881ad996c93412c5da779e8ceb20260
Akiva Grobman <akiva.grobman@gmail.com>
2024-04-01 11:15:09 +0300

    Update token

--- python-src/config.py
+++ python-src/config.py
@@ -5,4 +5,4 @@ from dataclasses import dataclass
 class Config:
     mongo_url = os.environ.get('MONGO_URL', 'mongodb+srv://israelitime.of5zepk.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority&appName=IsraeliTime')
     certs_path = os.environ.get('MONGO_CERT_PATH', '/home/ubuntu/.keys/mongo.pem')
-    student_token = ''
+    student_token = 'T0RZeE1qQTNNVGd4TXpRd05UUTVPVGc1TFRJd01ERXpORFkwTURjeU9EQTNNelUyTXpNd05qSTFOREEyT1RjME16UXdMakEwT0RBek9EZ3dORFUyT1Rjek5UazFNREF3T0RnM09Ua3lORFExTkRZM01EQXlOekEzTlRJMk5qY3VjajVm'
