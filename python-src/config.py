cde61ebd38b8a40e6a16a65ce38885b850766505
Akiva Grobman <akiva.grobman@gmail.com>
2024-04-01 09:39:45 +0300

    Add token

--- python-src/config.py
+++ python-src/config.py
@@ -5,3 +5,4 @@ from dataclasses import dataclass
 class Config:
     mongo_url = os.environ.get('MONGO_URL', 'mongodb+srv://israelitime.of5zepk.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority&appName=IsraeliTime')
     certs_path = os.environ.get('MONGO_CERT_PATH', '/home/ubuntu/.keys/mongo.pem')
+    student_token = ''
